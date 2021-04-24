d83fb08b30d33ab143a7c4e9a60d1e21fdb9560c
Erik-Tran-Simonsson-NTI-Johanneberg <erik.transimonsson@elev.ga.ntig.se>
2021-04-24 15:21:10 +0200

    Yardoc minor fix
    Wrote @params instead of @param.
    
    Now fixed.

--- model.rb
+++ model.rb
@@ -25,7 +25,7 @@ module Model
 
     # Outputs all data of a recipe
     # 
-    # @params [Integer] id, The ID of the recipe
+    # @param [Integer] id, The ID of the recipe
     # 
     # @return [Hash]
     #   * :recipe_id [Integer] The ID of the recipe
@@ -41,36 +41,36 @@ module Model
 
     # Creates a row in the recipes table
     # 
-    # @params [String] recipe_name, The name of the recipe
-    # @params [Integer] user_id, The ID of the user
-    # @params [String] date_created, The date of the recipe creation
-    # @params [String] ingredients, The ingredients of the recipe
-    # @params [Integer] instructions, The instructions of the recipe
+    # @param [String] recipe_name, The name of the recipe
+    # @param [Integer] user_id, The ID of the user
+    # @param [String] date_created, The date of the recipe creation
+    # @param [String] ingredients, The ingredients of the recipe
+    # @param [Integer] instructions, The instructions of the recipe
     def insert_into_recipes(recipe_name, user_id, date_created, ingredients, instructions)
         return get_db().execute("INSERT INTO recipes (recipe_name, user_id, date_created, ingredients, instructions) VALUES (?, ?, ?, ?, ?)", recipe_name, user_id, date_created, ingredients, instructions)
     end
 
     # Updates a row in the recipes table
     # 
-    # @params [String] recipe_name, The name of the recipe
-    # @params [Integer] user_id, The ID of the user
-    # @params [String] date_updated, The date of the latest recipe update
-    # @params [String] ingredients, The ingredients of the recipe
-    # @params [Integer] instructions, The instructions of the recipe
+    # @param [String] recipe_name, The name of the recipe
+    # @param [Integer] user_id, The ID of the user
+    # @param [String] date_updated, The date of the latest recipe update
+    # @param [String] ingredients, The ingredients of the recipe
+    # @param [Integer] instructions, The instructions of the recipe
     def update_recipes(recipe_name, date_updated, ingredients, instructions, recipe_id)
         return get_db().execute("UPDATE recipes SET recipe_name = ?, date_updated = ?, ingredients = ?, instructions = ? WHERE recipe_id = ?", recipe_name, date_updated, ingredients, instructions, recipe_id)
     end
 
     # Deletes a row from the recipes table
     # 
-    # @params [Integer] recipe_id, The ID of the recipe
+    # @param [Integer] recipe_id, The ID of the recipe
     def delete_recipe(recipe_id)
         return get_db().execute("DELETE FROM recipes WHERE recipe_id = ?", recipe_id)
     end
 
     # Outputs all user_ids that like a recipe
     # 
-    # @params [Integer] recipe_id, The ID of the recipe
+    # @param [Integer] recipe_id, The ID of the recipe
     # 
     # @return [Array] all of user_ids that liked the recipe
     def select_all_user_ids_from_like_id(recipe_id)
@@ -79,37 +79,37 @@ module Model
 
     # Adds an user_id into a row in the likes table
     # 
-    # @params [Integer] recipe_id, The ID of the recipe
-    # @params [Integer] user_id, The ID of the user
+    # @param [Integer] recipe_id, The ID of the recipe
+    # @param [Integer] user_id, The ID of the user
     def add_like(recipe_id, user_id)
         return get_db().execute("INSERT INTO likes (recipe_id, user_id) VALUES (?, ?)", recipe_id, user_id)
     end
 
     # Deletes an user_id from a row in the likes table
     # 
-    # @params [Integer] recipe_id, The ID of the recipe
-    # @params [Integer] user_id, The ID of the user
+    # @param [Integer] recipe_id, The ID of the recipe
+    # @param [Integer] user_id, The ID of the user
     def remove_like(recipe_id, user_id)
         return get_db().execute("DELETE FROM likes WHERE recipe_id = ? AND user_id = ?", recipe_id, user_id)
     end
 
     # Creates a row in the users table
     # 
-    # @params [String] username, The username of the user
-    # @params [String] password, The password of the user
+    # @param [String] username, The username of the user
+    # @param [String] password, The password of the user
     def create_new_user(username, password)
         return get_db().execute("INSERT INTO users (username, password) VALUES (?, ?)", username, password)
     end
 
     # Controls if the username exists within a row in the users table
-    # @params [String] username, The username of the user
+    # @param [String] username, The username of the user
     def username_exists(username)
         return get_db_as_hash().execute("SELECT * FROM users WHERE username = ?", username)
     end
 
     # Outputs all data of a user
     # 
-    # @params [Integer] username, The username of the user
+    # @param [Integer] username, The username of the user
     # 
     # @return [Hash]
     #   * :user_id [Integer] The ID of the user
